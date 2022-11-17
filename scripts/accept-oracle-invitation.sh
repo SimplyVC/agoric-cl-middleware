12d7d84796255a0038ff474d28dd436311323f26
Jacques Vella Critien <vellacritien@gmail.com>
2022-11-18 00:22:39 +0100

    Fixed accept oracle script + added grafana dashboard

--- scripts/accept-oracle-invitation.sh
+++ scripts/accept-oracle-invitation.sh
@@ -4,8 +4,10 @@ WALLET_NAME=$1
 BRAND_IN=$2
 BRAND_OUT=$3
 
+AGORIC_SDK=$(find ~ -type d -name "agoric-sdk" | head -n 1)
+
 WALLET_ADDR=$(agd keys show "$WALLET_NAME" --keyring-backend test --output json | jq -r .address)
-cd ../../agoric-cli
+cd $AGORIC_SDK/packages/agoric-cli
 ORACLE_OFFER=$(mktemp -t agops.XXX)
 bin/agops oracle accept --pair "$BRAND_IN.$BRAND_OUT" >|"$ORACLE_OFFER"
 cat $ORACLE_OFFER
@@ -14,7 +16,7 @@ bin/agoric wallet send --from "$WALLET_ADDR" --keyring-backend test --offer "$OR
 bin/agoric wallet show --from "$WALLET_ADDR"
 ORACLE_OFFER_ID=$(jq ".body | fromjson | .offer.id" <"$ORACLE_OFFER")
 echo "ORACLE_OFFER_ID: $ORACLE_OFFER_ID"
-cd ../agoric-cl-middleware/scripts
+cd ~/agoric-cl-middleware/scripts
 
 CONFIG_DIR=~/config
 OFFERS_FILE=$CONFIG_DIR/offers.json
