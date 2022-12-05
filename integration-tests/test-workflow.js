8d487c03c9c112bd116732dd32fd44de36aa4f27
Jacques Vella Critien <jacques@simply-vc.com.mt>
2022-12-05 16:53:02 +0100

    Cleaned code & added documentation

--- integration-tests/test-workflow.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { test } from '@agoric/swingset-vat/tools/prepare-test-env-ava.js';
-
-import { gettingStartedWorkflowTest } from '../tools/getting-started.js';
-
-test('workflow', t =>
-  gettingStartedWorkflowTest(t, { testUnsafePlugins: true }));
--- src/commands/helper.js
+++ src/commands/helper.js
@@ -1,21 +1,41 @@
 import fs from 'fs';
 import { URL } from 'url';
 
+/**
+ * Function to read a json file
+ * @param {*} filename  file name or path to read
+ * @returns the JSON data in the file
+ */
 export const readJSONFile = (filename) => {
     let rawdata = fs.readFileSync(filename);
     let data = JSON.parse(String(rawdata));
     return data
 }
 
+/**
+ * Function to read contents of a file
+ * @param {*} filename file name or path to read
+ * @returns the contents of the passed filename
+ */
 export const readFile = (filename) => {
     return fs.readFileSync(filename).toString();
 }
 
-export const saveState = (newState, filename) => {
-    let data = JSON.stringify(newState);
+/**
+ * Function to save JSON data to a file
+ * @param {*} newData new JSON data to save
+ * @param {*} filename filename to save data to
+ */
+export const saveJSONDataToFile = (newData, filename) => {
+    let data = JSON.stringify(newData);
     fs.writeFileSync(filename, data);
 }  
 
+/**
+ * Function to check whether a URL is valid or not
+ * @param {*} url the URL to check
+ * @returns whether the url is valid or not
+ */
 export const validUrl = (url) => {
     try {
     	new URL(url);
@@ -24,3 +44,10 @@ export const validUrl = (url) => {
       	return false;
     }
 };
+
+/**
+ * Function to create a delay
+ * @param {*} ms milliseconds to delay
+ * @returns a Promise to delay
+ */
+export const delay = (ms) => new Promise(res => setTimeout(res, ms));
