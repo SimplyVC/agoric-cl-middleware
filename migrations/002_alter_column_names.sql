40f68e5cd00ad47b7fc86cf1467f20902fc4d807
Jacques Vella Critien <jacques@simply-vc.com.mt>
2023-02-25 00:08:23 +0100

    Updated second migration

--- migrations/002_alter_column_names.sql
+++ migrations/002_alter_column_names.sql
@@ -13,17 +13,8 @@ RENAME COLUMN started_by TO startedBy;
 ALTER TABLE rounds
 RENAME COLUMN submission_made TO submissionMade;
 
-ALTER TABLE rounds
-RENAME COLUMN feed TO feed;
-ALTER TABLE rounds
-RENAME COLUMN roundId TO round_id;
-ALTER TABLE rounds
-RENAME COLUMN startedAt TO started_at;
-ALTER TABLE rounds
-RENAME COLUMN startedBy TO started_by;
-ALTER TABLE rounds
-RENAME COLUMN submissionMade TO submission_made;
-
 --------------------------------------------------------------------------------
 -- Down
 --------------------------------------------------------------------------------
+
+;
