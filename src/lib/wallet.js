0b2d652bdae553cc8801f3cf9e168e78721286d3
Jacques Vella Critien <jacques@simply-vc.com.mt>
2023-04-21 00:19:22 +0200

    Fixing getCurrent and the chain id in docker-compose

--- src/lib/wallet.js
+++ src/lib/wallet.js
@@ -15,5 +15,5 @@ export const getCurrent = async (addr, ctx, { vstorage }) => {
 
   const capDatas = storageHelper.unserializeTxt(capDataStr, ctx);
 
-  return capDatas[0];
+  return capDatas[-1];
 };
