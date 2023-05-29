0e44a5dc7aa9d3f778149532ce3802c9cfbe68e8
Jacques Vella Critien <jacques@simply-vc.com.mt>
2023-05-29 18:53:09 +0200

    Added process variable in accept script for rpc

--- scripts/accept-oracle-invitation.sh
+++ scripts/accept-oracle-invitation.sh
@@ -10,7 +10,7 @@ AGORIC_SDK=$(find ~ -type d -name "agoric-sdk" | head -n 1)
 
 WALLET_ADDR=$(agd keys show "$WALLET_NAME" --keyring-backend test --output json | jq -r .address)
 ORACLE_OFFER=$(mktemp -t agops.XXX)
-./src/bin-agops.js oracle accept --pair "$BRAND_IN.$BRAND_OUT" >|"$ORACLE_OFFER"
+AGORIC_RPC=$AGORIC_RPC ./src/bin-agops.js oracle accept --pair "$BRAND_IN.$BRAND_OUT" >|"$ORACLE_OFFER"
 cat $ORACLE_OFFER
 OFFER_TO_SEND=$(cat $ORACLE_OFFER | jq .)
 agd --chain-id=$CHAIN_ID --keyring-backend=test --from=$WALLET_ADDR tx swingset wallet-action --allow-spend "$OFFER_TO_SEND" --yes --node $AGORIC_RPC
